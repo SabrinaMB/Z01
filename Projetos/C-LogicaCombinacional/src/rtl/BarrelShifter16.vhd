08add2d5637fb3927da17eee55f5a5260c54f3b0
manucirne <manucirne@gmail.com>
2018-03-14 16:30:05 -0300

    abortando os barrels e finalizando dmux2 mux416

--- Projetos/C-LogicaCombinacional/src/rtl/BarrelShifter16.vhd
+++ Projetos/C-LogicaCombinacional/src/rtl/BarrelShifter16.vhd
@@ -9,20 +9,3 @@ entity BarrelShifter16 is
 			q:    out STD_LOGIC_VECTOR(15 downto 0));  -- output vector (shifted)
 end entity;
 
-architecture barrel8 of BarrelShifter8 is
-begin
-
-
-		q<= a when (size = "000" and dir = '0') else
-			a(14 downto 0) & a(15) when (size = "001" and dir = '0') else
-			a(13 downto 0) & a(15 downto 14) when (size = "010" and dir = '0') else
-			a(12 downto 0) & a(15 downto 13) when (size = "011" and dir = '0') else
-			a(11 downto 0) & a(15 downto 12) when (size = "100" and dir = '0') else
-			a when (size = "000" and dir = '1') else
-			a(0) & a(15 downto 1) when (size = "001" and dir = '1') else
-			a(1 downto 0) & a(15 downto 2) when (size = "010" and dir = '1') else
-			a(2 downto 0) & a(15 downto 3) when (size = "011" and dir = '1') else
-			a(3 downto 0) & a(15 downto 4) when (size = "100" and dir = '1');
-
-
-end barrel8;
--- Projetos/C-LogicaCombinacional/src/rtl/BarrelShifter8.vhd
+++ Projetos/C-LogicaCombinacional/src/rtl/BarrelShifter8.vhd
@@ -10,20 +10,3 @@ entity BarrelShifter8 is
 			q:    out STD_LOGIC_VECTOR(7 downto 0));  -- output vector (shifted)
 end entity;
 
-architecture barrel8 of BarrelShifter8 is
-begin
-
-
-		q<= a when (size = "000" and dir = '0') else
-			a(6 downto 0) & a(7) when (size = "001" and dir = '0') else
-			a(5 downto 0) & a(7 downto 6) when (size = "010" and dir = '0') else
-			a(4 downto 0) & a(7 downto 5) when (size = "011" and dir = '0') else
-			a(3 downto 0) & a(7 downto 4) when (size = "100" and dir = '0') else
-			a when (size = "000" and dir = '1') else
-			a(0) & a(7 downto 1) when (size = "001" and dir = '1') else
-			a(1 downto 0) & a(7 downto 2) when (size = "010" and dir = '1') else
-			a(2 downto 0) & a(7 downto 3) when (size = "011" and dir = '1') else
-			a(3 downto 0) & a(7 downto 4) when (size = "100" and dir = '1');
-
-
-end barrel8;
