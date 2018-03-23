de69414806c87a901bf6627919cadf347d2bbef0
PhelipeMuller <phelipe@navesmuller.com.br>
2018-03-23 17:58:50 -0300

    Funcionando e Testado

--- Projetos/E-LogicaSequencial/src/rtl/BinaryDigit.vhd
+++ Projetos/E-LogicaSequencial/src/rtl/BinaryDigit.vhd
@@ -2,9 +2,9 @@
 -- by Luciano Soares
 -- BinaryDigit.vhd
 
-Library ieee; 
+Library ieee;
 use ieee.std_logic_1164.all;
-  
+
 entity BinaryDigit is
 	port(
 		clock:   in STD_LOGIC;
@@ -13,3 +13,33 @@ entity BinaryDigit is
 		output: out STD_LOGIC
 	);
 end entity;
+
+architecture fds of BinaryDigit is
+
+  component Mux2Way is
+    port(
+      a: in STD_LOGIC;   -- entradas
+      b: in STD_LOGIC;   -- entradas
+      sel: in STD_LOGIC; -- Seletor
+      q: out STD_LOGIC   -- saida
+    );
+  end component;
+
+  component FlipFlopD is
+    port(
+      clock: in STD_LOGIC;   -- CLOCK
+      d: in STD_LOGIC; -- ?
+      clear: in STD_LOGIC;   -- saida
+      preset: in STD_LOGIC;   -- saida
+      q: out STD_LOGIC   -- saida
+    );
+  end component;
+
+  signal tmp,ph: STD_LOGIC;
+
+begin
+  mux: Mux2Way PORT MAP (ph,input,load,tmp);
+  flip: FlipFlopD PORT MAP (clock,tmp,'0','0',ph);
+  output <= ph;
+
+end architecture;
