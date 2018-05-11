5473c07c58e013a098d376f4a5d4a433e2dfd75b
emanuellemoco <emanuellesmoco@gmail.com>
2018-05-11 08:02:10 -0300

    memory io nao funcionando ainda

--- Projetos/G-Computador/src/rtl/MemoryIO.vhd
+++ Projetos/G-Computador/src/rtl/MemoryIO.vhd
@@ -9,6 +9,8 @@ entity MemoryIO is
         CLK_SLOW : IN  STD_LOGIC;
         CLK_FAST : IN  STD_LOGIC;
         RST      : IN  STD_LOGIC;
+        MUX_L    : IN  STD_LOGIC;
+        DMUX_L   : IN STD_LOGIC_VECTOR(2 downto 0);
 
         -- RAM 16K
         ADDRESS		: IN  STD_LOGIC_VECTOR (14 DOWNTO 0);
@@ -69,23 +71,65 @@ ARCHITECTURE logic OF MemoryIO IS
       );
   end component;
 
-<<<<<<< HEAD
-signal LOADSC LOADRAM STD_LOGIC;
+  component Register16 is
+    port(
+      clock:   in std_logic;
+      input:   in STD_LOGIC_VECTOR(15 downto 0);
+      load:    in std_logic;
+      output: out STD_LOGIC_VECTOR(15 downto 0)
+      );
+  end component;
+
+
+
+
+signal LOAD_REG: STD_LOGIC;
+signal LOAD_RAM: STD_LOGIC;
+signal LOAD_SCREEN: STD_LOGIC;
+signal DATA: STD_LOGIC_VECTOR(14 downto 0);
+signal INSTRUCTION: STD_LOGIC_VECTOR(14 downto 0);
+signal IN_REG: STD_LOGIC_VECTOR(14 downto 0);
 
 BEGIN
-LOADSC <= '1' when LOAD else -- ADDRESS(14) ou LOAD
-          '0';
+  
+  LOAD_RAM <= '1' when DMUX_L = "00";
+  LOAD_REG <= '1' when DMUX_L = "01";
+  LOAD_SCREEN <= '1' when DMUX_L = "10";
 
-LOADRAM <= '0' when LOAD else
-           '1';
+  RAM: RAM16K PORT MAP(
+  address => ADDRESS, 
+  clock => CLK_FAST, 
+  data => INPUT (14 downto 0),
+  wren => LOAD_RAM,
+  q => INSTRUCTION
+  );
+
+  REG: Register16 PORT MAP (
+    clock => CLK_SLOW,
+    input => INPUT(9 downto 0),
+    load => LOAD_REG,
+    output => LED
+    );
+
+  SCREEN_log: Screen PORT MAP(
+    INPUT => INPUT (14 downto 0),
+    LOAD => LOAD_SCREEN,
+    ADDRESS => ADDRESS(13 downto 0),
+    CLK_FAST =>  CLK_FAST,
+    CLK_SLOW => CLK_SLOW,
+    RST => RST,
+    LCD_INIT_OK => LCD_INIT_OK,
+    LCD_CS_N => LCD_CS_N,
+    LCD_D => LCD_D,
+    LCD_RESET_N => LCD_RESET_N,
+    LCD_RS => LCD_RS,
+    LCD_WR_N => LCD_WR_N
+    );
+
+  OUTPUT <= INSTRUCTION when MUX_L = '1';
+  OUTPUT<= SW when MUX_L = '0';
 
 
-RAM: RAM16K PORT MAP(ADDRESS(13 downto 0),CLK_FAST,INPUT,LOADRAM,OUTPUT);
-Sc: Screen PORT MAP(INPUT,LOADSC,ADDRESS(13 downto 0),CLK_FAST,CLK_SLOW,RST,LCD_INIT_OK,LCD_CS_N,LCD_D,LCD_RD_N,LCD_RESET_N,LCD_RS,LCD_WR_N)
-LED <= SW;
-=======
-BEGIN
 
 
->>>>>>> upstream/master
 END logic;
