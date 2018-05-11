b520716d74f00aec08e11a4b82fe2fcb77804415
emanuellemoco <emanuellesmoco@gmail.com>
2018-05-11 15:22:39 -0300

    memory io não testado pois não tem teste

--- Projetos/G-Computador/src/rtl/MemoryIO.vhd
+++ Projetos/G-Computador/src/rtl/MemoryIO.vhd
@@ -9,30 +9,28 @@ entity MemoryIO is
         CLK_SLOW : IN  STD_LOGIC;
         CLK_FAST : IN  STD_LOGIC;
         RST      : IN  STD_LOGIC;
-        MUX_L    : IN  STD_LOGIC;
-        DMUX_L   : IN STD_LOGIC_VECTOR(2 downto 0);
 
         -- RAM 16K
-        ADDRESS		: IN  STD_LOGIC_VECTOR (14 DOWNTO 0);
-        INPUT		: IN  STD_LOGIC_VECTOR (15 DOWNTO 0);
-        LOAD		: IN  STD_LOGIC ;
-        OUTPUT		: OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
+        ADDRESS   : IN  STD_LOGIC_VECTOR (14 DOWNTO 0);
+        INPUT   : IN  STD_LOGIC_VECTOR (15 DOWNTO 0);
+        LOAD    : IN  STD_LOGIC ;
+        OUTPUT    : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
 
         -- LCD EXTERNAL I/OS
         LCD_CS_N     : OUT   STD_LOGIC;
         LCD_D        : INOUT STD_LOGIC_VECTOR(15 downto 0);
         LCD_RD_N     : OUT   STD_LOGIC;
         LCD_RESET_N  : OUT   STD_LOGIC;
-        LCD_RS       : OUT   STD_LOGIC;	-- (DCx) 0 : reg, 1: command
+        LCD_RS       : OUT   STD_LOGIC; -- (DCx) 0 : reg, 1: command
         LCD_WR_N     : OUT   STD_LOGIC;
-        LCD_ON       : OUT   STD_LOGIC := '1';	-- liga e desliga o LCD
+        LCD_ON       : OUT   STD_LOGIC := '1';  -- liga e desliga o LCD
         LCD_INIT_OK  : OUT   STD_LOGIC;
 
         -- Switchs
         SW  : in std_logic_vector(9 downto 0);
         LED : OUT std_logic_vector(9 downto 0)
 
-		);
+    );
 end entity;
 
 
@@ -47,7 +45,7 @@ ARCHITECTURE logic OF MemoryIO IS
           -- Sistema
           CLK_FAST : IN  STD_LOGIC;
           CLK_SLOW : IN  STD_LOGIC;
-          RST 	   : IN  STD_LOGIC;
+          RST      : IN  STD_LOGIC;
 
           -- LCD EXTERNAL I/OS
           LCD_INIT_OK  : OUT STD_LOGIC;
@@ -55,7 +53,7 @@ ARCHITECTURE logic OF MemoryIO IS
           LCD_D        : INOUT STD_LOGIC_VECTOR(15 downto 0);
           LCD_RD_N     : OUT   STD_LOGIC;
           LCD_RESET_N  : OUT   STD_LOGIC;
-          LCD_RS       : OUT   STD_LOGIC;	-- (DCx) 0 : reg, 1: command
+          LCD_RS       : OUT   STD_LOGIC; -- (DCx) 0 : reg, 1: command
           LCD_WR_N     : OUT   STD_LOGIC
           );
   end component;
@@ -63,11 +61,11 @@ ARCHITECTURE logic OF MemoryIO IS
   component RAM16K IS
       PORT
       (
-          address	: IN STD_LOGIC_VECTOR (13 DOWNTO 0);
-          clock		: IN STD_LOGIC  := '1';
-          data		: IN STD_LOGIC_VECTOR (15 DOWNTO 0);
-          wren		: IN STD_LOGIC ;
-          q		   : OUT STD_LOGIC_VECTOR (15 DOWNTO 0)
+          address : IN STD_LOGIC_VECTOR (13 DOWNTO 0);
+          clock   : IN STD_LOGIC  := '1';
+          data    : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+          wren    : IN STD_LOGIC ;
+          q      : OUT STD_LOGIC_VECTOR (15 DOWNTO 0)
       );
   end component;
 
@@ -81,38 +79,45 @@ ARCHITECTURE logic OF MemoryIO IS
   end component;
 
 
-
-
-signal LOAD_REG: STD_LOGIC;
 signal LOAD_RAM: STD_LOGIC;
 signal LOAD_SCREEN: STD_LOGIC;
+signal LOAD_LED: STD_LOGIC;
+signal LOAD_SW: STD_LOGIC;
+
 signal DATA: STD_LOGIC_VECTOR(14 downto 0);
 signal INSTRUCTION: STD_LOGIC_VECTOR(14 downto 0);
 signal IN_REG: STD_LOGIC_VECTOR(14 downto 0);
 
 BEGIN
   
-  LOAD_RAM <= '1' when DMUX_L = "00";
-  LOAD_REG <= '1' when DMUX_L = "01";
-  LOAD_SCREEN <= '1' when DMUX_L = "10";
+--  LOAD_RAM <= '1' when DMUX_L = "00";
+--  LOAD_REG <= '1' when DMUX_L = "01";
+--  LOAD_SCREEN <= '1' when DMUX_L = "10";
+
+  LOAD_RAM <= LOAD when ADRESS < "0100000000000000" else '0';
+  LOAD_SCREEN <= LOAD when ADRESS < "0101001011000000" and  ADRESS > "0011111111111111" else '0';
+  LOAD_LED  <= LOAD when ADRESS = "0101001011000000" else '0';
+  LOAD_SW  <= LOAD when ADRESS = "0101001011000001" else '0';
+
 
   RAM: RAM16K PORT MAP(
   address => ADDRESS, 
   clock => CLK_FAST, 
-  data => INPUT (14 downto 0),
+  data => INPUT,
   wren => LOAD_RAM,
   q => INSTRUCTION
   );
 
   REG: Register16 PORT MAP (
     clock => CLK_SLOW,
-    input => INPUT(9 downto 0),
-    load => LOAD_REG,
+    input(9 downto 0) => INPUT(9 downto 0),
+    input(15 downto 0) => ("000000"),
+    load => LOAD_LED,
     output => LED
     );
 
   SCREEN_log: Screen PORT MAP(
-    INPUT => INPUT (14 downto 0),
+    INPUT => INPUT ,
     LOAD => LOAD_SCREEN,
     ADDRESS => ADDRESS(13 downto 0),
     CLK_FAST =>  CLK_FAST,
@@ -126,8 +131,9 @@ BEGIN
     LCD_WR_N => LCD_WR_N
     );
 
-  OUTPUT <= INSTRUCTION when MUX_L = '1';
-  OUTPUT<= SW when MUX_L = '0';
+
+  OUTPUT <= INSTRUCTION when LOAD_RAM;
+  OUTPUT<= SW when LOAD_SW;
 
 
 
